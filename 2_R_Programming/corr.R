a3d87b4044a13ef05154ee64a732622a086896dc
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-02-16 13:43:50 +1100

    updated corr.R script 16/02/2015
    updated function within corr.R script

--- 2_R_Programming/corr.R
+++ 2_R_Programming/corr.R
@@ -10,4 +10,16 @@ corr <- function(directory, threshold = 0) {
   
 ## Returns a numeric vector of correlations
 
+files <- (Sys.glob("specdata//*.csv"));
+correlations <- c()
+  
+  for (file in files) {
+      file_data <- read.csv(file, sep = ",");
+      complete_cases <- file_data[complete.cases(file_data),];
+      if (nrow(complete_cases) > threshold) {
+          correlations <- c(correlations, cor(complete_cases$sulfate, complete_cases$nitrate))
+      }
+  }
+  
+return(correlations)    
 }
