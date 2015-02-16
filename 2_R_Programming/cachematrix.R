0e2cbb7446186f98c9dd31f7e912a9d3fb250301
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-02-16 13:35:20 +1100

    Create cachematrix.R
    created cachematrix.R script

---
+++ 2_R_Programming/cachematrix.R
@@ -0,0 +1,11 @@
+## The following pair of functions caches and computes the
+## inverse of a matrix.
+
+## This function creates a special "matrix" object that can cache its inverse.
+
+
+
+## This function computes the inverse of the special "matrix" returned by `makeCacheMatrix` above. 
+## If the inverse has already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
+
